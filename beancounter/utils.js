39bb18a3ee764ef4e5f22ed360e7ee3fd4dec22b
Libby Miller <libby@nicecupoftea.org>
2012-03-07 11:13:01 +0000

    improving date text formatting

--- beancounter/utils.js
+++ beancounter/utils.js
@@ -1,7 +1,8 @@
 
 //config
-var bc_url ="http://moth.notube.tv/notube-platform/rest/";
-var app_key = "ecb1e9fc90d14b63a3fe70497cbf1d69";
+var bc_url ="http://localhost/~elizabeth/notube-platform/rest/";
+var app_key = "b21f371ea80942b7b6f5346e9761f814";
+var return_part = "localhost/~elizabeth/beancounter_ui/beancounter/";
 var n_screen_api_root="http://nscreen.notu.be/iplayer_dev/api/";
 
 //open and close the settings button
@@ -29,16 +30,25 @@ function process_date(d){
 
   var a_week_ago = new Date();
   a_week_ago.setDate(a_week_ago.getDate() - 7);
+console.log("a week ago "+a_week_ago);
+
+  var yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
 
   var date_text = "";
 
   if(then.getDate()==now.getDate() && then.getMonth()==now.getMonth() && then.getYear()==now.getYear()){
+//i.e. today
     var hrs = now.getHours() - then.getHours();
     date_text = ""+parseInt(hrs)+" hours ago";
   }else{
 
     if(then > a_week_ago){
-      date_text = "Last "+days[then.getDay()];
+      if(then > yesterday){
+       date_text = "Yesterday";
+      }else{
+       date_text = "On "+days[then.getDay()];
+      }
     }else{
       var yr = then.getYear()+1900;
       date_text = days[then.getDay()]+", "+then.getDate()+" "+months[then.getMonth()]+" "+yr;
